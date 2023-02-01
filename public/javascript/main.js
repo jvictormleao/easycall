ad8a6e4758757e82deea1408370ef2a32072e1c6
jvictormleao <joaovictormarianoleao@gmail.com>
2023-02-01 10:59:00 -0300

    menu ok

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,43 @@
-// Abre e fecha a aba do menu
+// Abre e fecha menu
+const menu = document.querySelector('nav .icon-menu1')
+const close = document.querySelector('nav .icon-close')
+const nav = document.querySelector('nav ul.menu')
+const items = document.querySelectorAll('nav ul li.items')
+
+menu.addEventListener('click', function () {
+  nav.classList.add('show')
+  menu.classList.add('hidden')
+  close.classList.add('show')
+  for (const item of items) {
+    item.classList.add('show')
+  }
+})
+
+close.addEventListener('click', function () {
+  nav.classList.remove('show')
+  menu.classList.remove('hidden')
+  close.classList.remove('show')
+  for (const item of items) {
+    item.classList.remove('show')
+  }
+})
+
+const abas = document.querySelectorAll('main section')
+//Abre e fecha abas do menu
+for (const item of items) {
+  for (const aba of abas) {
+    item.addEventListener('click', function () {
+      if (item.id === aba.id) {
+        aba.classList.toggle('show')
+        nav.classList.toggle('show')
+        close.classList.toggle('show')
+        menu.classList.toggle('hidden')
+      } else {
+        aba.classList.remove('show')
+        nav.classList.toggle('show')
+        close.classList.toggle('show')
+        menu.classList.toggle('hidden')
+      }
+    })
+  }
+}
